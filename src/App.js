f58e3ed82836d2750700346fbe1c8ac7a89ae389
Reutg <48221955+Reutg@users.noreply.github.com>
2019-11-24 12:43:06 +0200

    material ui set up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import { observer } from 'mobx-react'
-import './App.css';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
-import React, { Component } from 'react';
+import Navbar from './components/Navbar'
+
+import './App.css'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#C38D9E',
+    },
+    secondary: {
+      main: '#41B3A3',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+    fontFamily: '"Montserrat", sans-serif',
+  },
+})
 
 @observer
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    );
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <Navbar />
+          {/* <Route
+            exact
+            path='/'
+            render={({ match }) => <Main match={match} />}
+          /> */}
+        </MuiThemeProvider>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
