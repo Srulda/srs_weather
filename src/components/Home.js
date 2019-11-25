4b5e8bb9a83179c0906238870913ccee6c1bc8de
Reutg <48221955+Reutg@users.noreply.github.com>
2019-11-25 23:36:20 +0200

    update styling for search input

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,70 @@
-import React, { Component } from "react";
-import { observer, inject } from "mobx-react";
-import CurrentWeather from "./CurrentWeather";
-import SearchInput from "./SearchInput";
-import AutoComplete from "./AutoComplete";
+import React, { Component } from 'react'
+import { observer, inject } from 'mobx-react'
+import CurrentWeather from './CurrentWeather'
+import SearchInput from './SearchInput'
+import AutoComplete from './AutoComplete'
 
 @observer
-@inject("weatherStore")
+@inject('weatherStore')
 class Home extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      searchInput: "",
-      isCityDisplay : false
-    };
+      searchInput: '',
+      isCityDisplay: false,
+      anchorEl: null,
+    }
   }
 
-  displayCities =  async (e) =>{
+  displayCities = async e => {
     await this.inputHandler(e)
     await this.props.weatherStore.displayFilteredData(this.state.searchInput)
-     this.setState({ isCityDisplay : true})
-
+    this.setState({ isCityDisplay: true }, () => {})
   }
 
   inputHandler = e => {
-    this.setState( {[e.target.id] : e.target.value} )
+    this.setState({ [e.target.id]: e.target.value })
   }
 
+  openPopper = event => {
+    const { anchorEl } = this.state
+    this.setState({ anchorEl: anchorEl ? null : event.currentTarget })
+  }
+
+  closePopper = () => {
+    this.setState({ isCityDisplay: false })
+  }
+
+  updateSelectedToInput = (city, country) => {
+    this.setState({ searchInput: `${city}, ${country}` })
+  }
 
   render() {
+    const { searchInput, isCityDisplay, anchorEl } = this.state
+
     return (
       <div>
+        <SearchInput
+          displayCities={this.displayCities}
+          inputHandler={this.inputHandler}
+          searchInput={searchInput}
+          openPopper={this.openPopper}
+        />
+        {/* <button onClick={this.getWeather}>search</button> */}
         <div>
-
-      <SearchInput/>
-      <CurrentWeather/>
-
-          <input type="text"
-          id = "searchInput"
-          value = {this.state.searchInput}
-          onChange = {this.displayCities} 
-           />
-          <button onClick = {this.getWeather}>search</button>
-
+          {isCityDisplay ? (
+            <AutoComplete
+              open={isCityDisplay}
+              anchorEl={anchorEl}
+              updateSelectedToInput={this.updateSelectedToInput}
+              closePopper={this.closePopper}
+            />
+          ) : null}
         </div>
-        <div>{ this.state.isCityDisplay ? <AutoComplete /> : null} </div>
+        <CurrentWeather />
       </div>
-    );
+    )
   }
 }
 
-export default Home;
-
-
+export default Home
